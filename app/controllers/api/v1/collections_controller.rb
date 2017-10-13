4572287413b3ccd0a6a0a97fcbdd3bcb5635d85f
imletaconnoux <imletaconnoux@gmail.com>
2017-10-13 11:48:48 -0400

    added collection delete and edit actions

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -11,9 +11,24 @@ class Api::V1::CollectionsController < ApplicationController
   end
 
   def show
-    @collection = Collection.find(params.id)
+
+    @collection = Collection.find(params[:id])
     @videos = @collection.videos
     render json: @collection, status: 201
+  end
+
+  def destroy
+    @collection = Collection.find_by(id: params[:id])
+    @collection.destroy
+    render json: @collection
+  end
+
+  def update
+    @collection = Collection.find_by(id: params[:id])
+    @collection.name = params[:name]
+    @collection.save
+    render json: @collection
+
   end
 
   private
