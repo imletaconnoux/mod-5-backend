06c7fd8368f4aa6663a1087e87feda941a60730b
imletaconnoux <imletaconnoux@gmail.com>
2017-10-13 17:10:04 -0400

    added controller action for removing a video from a collection

--- app/controllers/api/v1/video_collections_controller.rb
+++ app/controllers/api/v1/video_collections_controller.rb
@@ -10,6 +10,13 @@ class Api::V1::VideoCollectionsController < ApplicationController
     render json: @video_collection, status: 201
   end
 
+  def destroy
+    @video_collection = VideoCollection.find_by(collection_id: params[:collection_id], video_id: params[:video_id])
+    @video_collection.destroy
+    render json: @video_collection
+
+  end
+
   private
   def video_collection_params
     params.permit(:collection_id, :video_id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :collections
       resources :video_collections
+      delete '/video_collections', to: 'video_collections#destroy'
       resources :videos
       resources :searches
     end
