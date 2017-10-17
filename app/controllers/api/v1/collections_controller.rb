9e62d4dd913b1417146388b298678c8fbf4ddbcc
imletaconnoux <imletaconnoux@gmail.com>
2017-10-17 10:24:59 -0400

    added current user for viewing collections and creating

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -1,19 +1,23 @@
 class Api::V1::CollectionsController < ApplicationController
 
   def index
-    @collections = Collection.all
+    #JWT token has encoded user_id
+    #frontend makes get request with JWT token
+    #decode token and find collections where user_id = decoded_token_id
+
+    @collections = current_user.collections
     @collections.map do | collection |
       if collection.videos.length > 0
         collection.image = collection.videos[0].thumbnail
         collection.save
       end
     end
-    
+
     render json: @collections, status: 200
   end
 
   def create
-    @collection = Collection.create(collection_params)
+    @collection = Collection.create(user_id: current_user.id, name: params[:name])
 
     render json: @collection, status: 201
   end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       resources :searches
     end
   end
-
+  post '/login', to: 'auth#create'
+  get '/welcome', to: 'application#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
